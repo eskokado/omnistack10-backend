ac5c48bd14dd03a3e28611a51ad1102de7b4817d
Edson Shideki Kokado <eskokado@gmail.com>
2020-01-14 08:37:03 -0300

    Configurando formato JSON no express

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+// Query Params: request.query (Filtros, ordenação, paginação...)
+// Route Params: request.params (Identificar um recurso na alteração | remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+app.post('/users', (request, response) => {
+    console.log(request.body)
     return response.json( { message: 'Hello OmniStack' });
 });
 
