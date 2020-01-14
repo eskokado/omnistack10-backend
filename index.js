d1fa3ae590c8277064d694dbf310b9eb1b91cf5d
Edson Shideki Kokado <eskokado@gmail.com>
2020-01-14 09:11:17 -0300

    Configurando mongoose

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-llinr.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(express.json());
 
@@ -10,6 +16,8 @@ app.use(express.json());
 // Query Params: request.query (Filtros, ordenação, paginação...)
 // Route Params: request.params (Identificar um recurso na alteração | remoção)
 // Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
 
 app.post('/users', (request, response) => {
     console.log(request.body)
